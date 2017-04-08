ba3ae2b538f8fbed67a04e7659cb485a57f08b2c
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-04-08 11:41:42 +0900

    fix

--- script/auth-gcloud.sh
+++ script/auth-gcloud.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-echo $GCLOUD_KEY | base64 --decode > gcloud.p12
-gcloud auth activate-service-account $GCLOUD_EMAIL --key-file=gcloud.p12
+# echo $GCLOUD_KEY | base64 --decode > gcloud.p12
+echo $GCLOUD_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+# gcloud auth activate-service-account $GCLOUD_EMAIL --key-file=gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
